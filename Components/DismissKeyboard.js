6ce2de9285030b82e72e2507ec1d6e7426f88755
Railrik <benjamin@ligny.pro>
2023-10-08 10:11:59 +0200

    ✨add dismiss keyboard function when click out

---
+++ Components/DismissKeyboard.js
@@ -0,0 +1,15 @@
+import {
+    Keyboard,
+    TouchableWithoutFeedback
+} from 'react-native'
+
+const DismissKeyboard = ({ children }) => {
+    return (
+        <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
+            {children}
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default DismissKeyboard
+
